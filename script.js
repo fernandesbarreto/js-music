243e0542f1de25e789766381b5d95a690e5f649a
fernandesbarreto <78365783+fernandesbarreto@users.noreply.github.com>
2022-03-01 15:29:30 -0300

    someChords

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const BRANCAS = ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i'];
 const PRETAS = ['s', 'd', 'g', 'h', 'j', '2', '3', '5', '6', '7'];
+const ACORDES = ['p', 'ç', '.', ';', '~'];
 
 const keys = document.querySelectorAll('.tecla');
 const teclaB = document.querySelectorAll('.tecla.branca');
 const teclaP = document.querySelectorAll('.tecla.preta');
 
 keys.forEach(tecla => {
-    tecla.addEventListener('click', () => playNote(tecla), 'mouse');
+    tecla.addEventListener('click', () => playNote(tecla));
 })
 
 document.addEventListener('keydown', e => {
@@ -14,12 +15,15 @@ document.addEventListener('keydown', e => {
     const tecla = e.key;
     const brancaIndex = BRANCAS.indexOf(tecla);
     const pretaIndex = PRETAS.indexOf(tecla);
+    const acordeIndex = ACORDES.indexOf(tecla);
 
-    if(brancaIndex > -1) playNote(teclaB[brancaIndex], 'teclado')
-    if(pretaIndex > -1) playNote(teclaP[pretaIndex], 'teclado')
+    if(brancaIndex > -1) playNote(teclaB[brancaIndex])
+    if(pretaIndex > -1) playNote(teclaP[pretaIndex])
+    if(acordeIndex > -1) acorde(acordeIndex);
+
 })
 
-function playNote(tecla, entrada){
+function playNote(tecla){
     const som = document.getElementById(tecla.dataset.note);
     som.currentTime = 0;
     som.play();
@@ -28,4 +32,17 @@ function playNote(tecla, entrada){
         tecla.classList.remove('ativo');
        }
     )
+}
+
+function acorde(toca){
+    if(toca===0){
+        playNote(teclaB[0]);
+        playNote(teclaB[2]);
+        playNote(teclaB[4]);
+    }
+    else if(toca===1){
+        playNote(teclaB[1]);
+        playNote(teclaB[3]);
+        playNote(teclaB[5]);
+    }
 }
