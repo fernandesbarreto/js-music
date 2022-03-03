4f42d1d4c80c2d58ae0ca628ccaa400b386edcaa
fernandesbarreto <78365783+fernandesbarreto@users.noreply.github.com>
2022-03-03 20:41:12 -0300

    Ear Training

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ const ACORDES = ['p', 'ç', '.', ';', '~'];
 const keys = document.querySelectorAll('.tecla');
 const teclaB = document.querySelectorAll('.tecla.branca');
 const teclaP = document.querySelectorAll('.tecla.preta');
+const botao = document.getElementById('botao');
+
 
 keys.forEach(tecla => {
-    tecla.addEventListener('click', () => playNote(tecla));
+    tecla.condicao=0;
+    tecla.addEventListener('click', () => playNote(tecla, tecla.condicao));
 })
+
+
+botao.addEventListener('click', () => {
+    let random = Math.floor((Math.random()*teclaB.length));
+    playTeste(teclaB[random]);
+}
+);
+
 
 document.addEventListener('keydown', e => {
     if (e.repeat) return
@@ -17,21 +28,62 @@ document.addEventListener('keydown', e => {
     const pretaIndex = PRETAS.indexOf(tecla);
     const acordeIndex = ACORDES.indexOf(tecla);
 
-    if(brancaIndex > -1) playNote(teclaB[brancaIndex])
-    if(pretaIndex > -1) playNote(teclaP[pretaIndex])
-    if(acordeIndex > -1) acorde(acordeIndex);
+    if(brancaIndex > -1) playNote(teclaB[brancaIndex], 0)
+    if(pretaIndex > -1) playNote(teclaP[pretaIndex], 0)
+    if(acordeIndex > -1) acorde(acordeIndex, 0)
 
 })
 
-function playNote(tecla){
+function playNote(tecla, teste){
     const som = document.getElementById(tecla.dataset.note);
     som.currentTime = 0;
     som.play();
-    tecla.classList.add('ativo');
-    som.addEventListener('ended', () => {
-        tecla.classList.remove('ativo');
-       }
-    )
+
+    if(teste==1){
+        tecla.classList.add('certo');
+        som.addEventListener('ended', () => {
+            tecla.classList.remove('certo');
+           }
+        )
+        tecla.condicao=0;
+    }else if(teste==2){
+        tecla.classList.add('errado');
+        som.addEventListener('ended', () => {
+            tecla.classList.remove('errado');
+           }
+        )
+    }else{
+        tecla.classList.add('ativo');
+        som.addEventListener('ended', () => {
+            tecla.classList.remove('ativo');
+        }
+        )
+    }
+
+    keys.forEach(nota => {
+        if(nota.condicao==1){
+            tecla.classList.add('errado');
+            som.addEventListener('ended', () => {
+                tecla.classList.remove('errado');
+           }
+        )
+        }
+        nota.condicao=0;
+    })
+}
+
+function playTeste(tecla){
+    const som = document.getElementById(tecla.dataset.note);
+    som.currentTime = 0;
+    som.play();
+    testando(tecla);
+}
+
+function testando(tecla){
+    keys.forEach(nota => {
+        nota.condicao=2;
+    })
+    tecla.condicao=1;
 }
 
 function acorde(toca){
